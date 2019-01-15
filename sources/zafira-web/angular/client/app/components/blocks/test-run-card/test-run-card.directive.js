8b28895c1a62fe411a3fc5714c76136fc342cc43
Danymill <justeam@gmail.com>
2019-01-15 12:34:12 +0300

    in progress: test-run-card functionality

--- sources/zafira-web/angular/client/app/components/blocks/test-run-card/test-run-card.directive.js
+++ sources/zafira-web/angular/client/app/components/blocks/test-run-card/test-run-card.directive.js
@@ -5,7 +5,8 @@
         return {
             templateUrl: 'app/components/blocks/test-run-card/test-run-card.html',
             controller: function TestRunCardController(mediaBreakpoints, windowWidthService,
-                                                       testsRunsService, $rootScope, UtilService) {
+                                                       testsRunsService, $rootScope, UtilService,
+                                                       $state, $timeout, $mdDialog, $mdToast) {
                 var vm = {
                     testRun: null,
                     singleMode: false,
@@ -18,10 +19,17 @@
                     isSlackAvailable: false,
                     slackChannels: null,
                     currentOffset: $rootScope.currentOffset,
+                    tools: $rootScope.tools,
 
                     addToSelectedtestRuns: addToSelectedtestRuns,
                     showDetails: showDetails,
                     openMenu: openMenu,
+                    openTestRun: openTestRun,
+                    copyLink: copyLink,
+                    markAsReviewed: markAsReviewed,
+                    showCommentsDialog: showCommentsDialog,
+                    sendAsEmail: sendAsEmail,
+                    createSpreadsheet: createSpreadsheet,
                 };
 
                 vm.$onInit = init;
@@ -54,7 +62,6 @@
                 }
 
                 function addToSelectedtestRuns() {
-                    console.dir(vm.onSelect);
                     vm.onSelect && vm.onSelect(vm.testRun);
                 }
 
@@ -71,8 +78,100 @@
                 function openMenu($event, $msMenuCtrl) {
                     initMenuRights();
                     UtilService.setOffset($event);
-                    vm.currentOffset = $rootScope.currentOffset;
-                    $msMenuCtrl.open($event);
+                    $timeout(function() {
+                        vm.currentOffset = $rootScope.currentOffset;
+                        $msMenuCtrl.open($event);
+                    });
+                }
+
+                function openTestRun() {
+                    const url = $state.href('tests/run2', {testRunId: vm.testRun.id});
+
+                    window.open(url,'_blank');
+                }
+
+                function copyLink() {
+                    const url = $state.href('tests/run2', {testRunId: vm.testRun.id}, {absolute : true});
+
+                    url.copyToClipboard();
+                }
+
+                function markAsReviewed() {
+                    showCommentsDialog();
+                }
+
+                function sendAsEmail(event) {
+                    showEmailDialog([vm.testRun], event);
+                }
+
+                function createSpreadsheet(event) {
+                    showCreateSpreadsheetDialog([vm.testRun], event);
+                };
+
+                function showCommentsDialog(event) {
+                    $mdDialog.show({
+                        controller: 'CommentsController',
+                        templateUrl: 'app/components/modals/comments/comments.html',
+                        parent: angular.element(document.body),
+                        targetEvent: event,
+                        clickOutsideToClose:true,
+                        fullscreen: true,
+                        locals: {
+                            testRun: vm.testRun,
+                            isSlackAvailable: vm.isSlackAvailable,
+                            slackChannels: vm.slackChannels
+                        }
+                    }).then(function(answer) {
+                        vm.testRun.reviewed = answer.reviewed;
+                        vm.testRun.comments = answer.comments;
+                    });
+                }
+
+                function showEmailDialog(testRuns, event) {
+                    $mdDialog.show({
+                        controller: 'EmailController',
+                        templateUrl: 'app/components/modals/email/email.html',
+                        parent: angular.element(document.body),
+                        targetEvent: event,
+                        clickOutsideToClose:true,
+                        fullscreen: true,
+                        locals: {
+                            testRuns: testRuns
+                        }
+                    });
+                }
+
+                function showCreateSpreadsheetDialog(testRuns, event) {
+                    $mdDialog.show({
+                        controller: 'SpreadsheetController',
+                        templateUrl: 'app/components/modals/spreadsheet/spreadsheet.html',
+                        parent: angular.element(document.body),
+                        targetEvent: event,
+                        clickOutsideToClose:true,
+                        fullscreen: true,
+                        locals: {
+                            testRuns: testRuns
+                        }
+                    })
+                    .then(undefined, function(links) {
+                        if (links.length) {
+                            $mdToast.show({
+                                hideDelay: 0,
+                                position: 'bottom right',
+                                locals: {
+                                    links: links
+                                },
+                                controller: function ($scope, $mdToast, links) {
+                                    $scope.links = links;
+
+                                    $scope.closeToast = function () {
+                                        $mdToast.hide();
+                                    };
+                                },
+                                templateUrl: 'app/_testruns/links_toast.html'
+                            });
+                        }
+                    });
                 }
             },
             scope: {
